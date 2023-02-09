c3d0c36d0120fd54877afc7c21ad258da84a8b67
Mike Ramos <michaelangeloram8@gmail.com>
2023-02-09 11:44:19 -0600

    Add styling: addressContainer and addressTitle

--- src/screens/LandingScreen.tsx
+++ src/screens/LandingScreen.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 import { View, Text, StyleSheet, Dimensions, Image } from 'react-native';
 
+const screenWidth = Dimensions.get('screen').width
+
 export const LandingScreen = () => {
+    const { addressContainer } = styles;
+
     return (
         <View style={styles.container}>
-            <View style={styles.navigation}>
-                <Text>Navigation</Text>
-            </View>
+            <View style={styles.navigation} />
+            
             <View style={styles.body}>
-                <Text>Landing Screen</Text>
+                <Image source={require('../images/delivery_icon.png')} style={styles.deliveryIcon} />
+                <View style={styles.addressContainer}>
+                    <Text style={styles.addressContainer}>Your delivery address</Text>
+                </View>
             </View>
             <View style={styles.footer}>
                 <Text>Footer</Text>
             </View>
         </View>
+      
     )
 };
 
@@ -33,11 +40,30 @@ const styles = StyleSheet.create({
         flex: 9,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'yellow',
     },
 
     footer: {
         flex: 1,
         backgroundColor: 'cyan',
+    },
+
+    deliveryIcon: {
+        width: 120,
+        height: 120,
+    },
+
+    addressContainer: {
+        width: screenWidth - 100,
+        borderBottomColor: 'red',
+        borderBottomWidth: 0.5,
+        padding: 5,
+        marginBottom: 10,
+        alignItems: 'center',
+    },
+
+    addressTitle: {
+        fontSize: 24,
+        fontWeight: '700',
+        color: '#7d7d7d',
     }
 });
