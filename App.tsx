b9963c695642cd064eafe9b292c47d851ecfd229
Mike Ramos <michaelangeloram8@gmail.com>
2023-02-10 14:59:46 -0600

    Convert switch to RootStack: stackNavigator instea

--- App.tsx
+++ App.tsx
@@ -8,7 +8,8 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 
-const switchNavigator = createSwitchNavigator({
+const RootStack = createStackNavigator({
+
   landingStack: {
     screen: createStackNavigator({
       Landing: LandingScreen,
