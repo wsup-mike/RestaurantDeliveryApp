36d739f5a6ad4e07801c804e94452c1089b443f6
Mike Ramos <michaelangeloram8@gmail.com>
2023-02-10 09:26:57 -0600

    LandingScreen: create useEffect

--- src/screens/LandingScreen.tsx
+++ src/screens/LandingScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useReducer } from "react";
+import React, { useState, useReducer, useEffect } from "react";
 import { View, Text, StyleSheet, Dimensions, Image } from 'react-native';
 
 import * as Location from 'expo-location';
@@ -6,7 +6,41 @@ import * as Location from 'expo-location';
 const screenWidth = Dimensions.get('screen').width
 
 export const LandingScreen = () => {
-    const { addressContainer } = styles;
+    // const { addressContainer } = styles;
+    const [errorMsg, setErrorMsg] = useState("");
+    const [address, setAddress] = useState<Location.LocationGeocodedAddress>();
+
+    const [displayAddress, setDisplayAddress] =  useState("Waiting for current location");
+
+    useEffect(() => {
+        (async() => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+
+            if ( status !== 'granted') {
+                setErrorMsg('Permission to access device location not granted')
+            }
+
+            let location: any = await Location.getCurrentPositionAsync({});
+
+            const { coords } = location;
+
+            if (coords) {
+                const { latitude, longitude } = coords;
+
+                let addressResponse: any = await Location.reverseGeocodeAsync({ latitude, longitude })
+
+                for(let item of addressResponse) {
+                    setAddress(item)
+                    let currentAddress = `${item.name}, ${item.street}, ${item.postalCode}, ${item.country}`
+                    setDisplayAddress(currentAddress)
+                    return;
+                }
+
+            } else {
+                //notify user something went wrong with location
+            }
+        })
+    }, [])
 
     return (
         <View style={styles.container}>
@@ -17,7 +51,7 @@ export const LandingScreen = () => {
                 <View style={styles.addressContainer}>
                     <Text style={styles.addressContainer}>Your delivery address</Text>
                 </View>
-                <Text style={styles.addressText}>Waiting for current location</Text>
+                <Text style={styles.addressText}>{displayAddress}</Text>
             </View>
             <View style={styles.footer}>
                 <Text>Footer</Text>
