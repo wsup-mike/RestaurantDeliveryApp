62608931bc955df09af4acf35ea4cb25a52c21f7
Mike Ramos <michaelangeloram8@gmail.com>
2023-02-10 10:33:35 -0600

    Create switchNavigator (deprecated)

--- App.tsx
+++ App.tsx
@@ -1,10 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { HomeScreen } from './src/screens/HomeScreen';
 import { LandingScreen } from './src/screens/LandingScreen';
 
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
+
+const switchNavigator = createSwitchNavigator({
+  landingStack: {
+    screen: createStackNavigator({
+      Landing: LandingScreen,
+      // search address screen
+    }, {
+      defaultNavigationOptions: {
+        headerShown: false
+      }
+    })
+  },
+
+  homeStack: createBottomTabNavigator({
+    //Home tab icon
+    home: {
+      screen: createStackNavigator({
+        HomePage: HomeScreen
+      }),
+      navigationOptions: {
+        tabBarIcon: ({ focused, tintColor }) => {
+          let icon = focused == true ? require('./src/images/home_icon.png') : require('./src/images/home_n_icon.png')
+          return <Image source={icon} style={styles.tabIcon} />
+        }
+      }
+    }
+  })
+
+});
 
 export default function App() {
   return (
@@ -21,4 +52,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  tabIcon: {
+    width: 30,
+    height: 30,
+  }
 });
