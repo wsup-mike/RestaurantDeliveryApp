3000f43b5a0859393b78ce5e3f17fc71b307f884
Mike Ramos <michaelangeloram8@gmail.com>
2023-02-13 10:34:18 -0600

    Create custom hook useNavigation

--- src/utils/useNavigation.tsx
+++ src/utils/useNavigation.tsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
-import { NavigationScreenProp, NavigationRoute, NavigationContext, NavigationParams } from '@react-navigation/native';
+import { NavigationContext } from '@react-navigation/native';
 
 export function useNavigation() {
-    return useContext(NavigationContext) as NavigationScreenProp<NavigationRoute, NavigationParams>
+    return useContext(NavigationContext);
 }
