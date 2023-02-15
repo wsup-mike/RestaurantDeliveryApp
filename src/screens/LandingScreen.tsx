1815a018f0c93d58e90699c1a2716e935ea30e7c
Mike Ramos <michaelangeloram8@gmail.com>
2023-02-14 22:48:38 -0600

    another attempt

--- src/screens/LandingScreen.tsx
+++ src/screens/LandingScreen.tsx
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet, Dimensions, Image } from 'react-native';
 import { ParamListBase, useNavigation } from "@react-navigation/native";
 import * as Location from 'expo-location';
 import { StackNavigationProp } from "@react-navigation/stack";
-import { HomeStackScreen, HomeTabsScreen } from "../../App";
+import { HomeStackScreen } from "../../App";
 
 const screenWidth = Dimensions.get('screen').width
 
