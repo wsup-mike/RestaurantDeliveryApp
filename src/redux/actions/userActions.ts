83dd311ac3d87a7db81d9bcb169622b445291dfe
Mike Ramos <michaelangeloram8@gmail.com>
2023-02-19 11:51:43 -0600

    Create interface UpdateLocationAction

--- src/redux/actions/userActions.ts
+++ src/redux/actions/userActions.ts
@@ -2,3 +2,8 @@ import axios from "axios";
 import { Dispatch } from "react";
 import { BASE_URL } from "../../utils";
 import { LocationGeocodedAddress } from "expo-location";
+
+export interface UpdateLocationAction {
+    readonly type: 'ON_UPDATE_LOCATION',
+    payload: LocationGeocodedAddress,
+}
