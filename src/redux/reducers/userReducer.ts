f8fb16a75aa85c67827409a99202feec88024572
Mike Ramos <michaelangeloram8@gmail.com>
2023-02-19 12:30:09 -0600

    userReducer-create new object initialState

--- src/redux/reducers/userReducer.ts
+++ src/redux/reducers/userReducer.ts
@@ -1,5 +1,13 @@
-import { UserAction} from '../actions';
-import { UserState } from '../models';
+import { LocationGeocodedAddress } from 'expo-location';
+import { UserAction } from '../actions';
+import { UserModel, UserState } from '../models';
+
+const initialState: UserState = {
+    user: {} as UserModel,
+    location: {} as LocationGeocodedAddress,
+    error: undefined,
+}
+
 
 const UserReducer = () => {
 
