02d4c29fbdbdf4b268c3d1b7de284fa88ebb9fc9
Mike Ramos <michaelangeloram8@gmail.com>
2023-02-19 11:12:29 -0600

    Create our new models in index.ts in models folder

---
+++ src/redux/models/index.ts
@@ -0,0 +1,29 @@
+// To create interfaces ('Models!') for our final data object (Define this object shape)
+
+//Category Model
+export interface Category {
+    title: string,
+    icon: string,
+}
+
+//Food Model
+export interface FoodModel {
+    _id: string;
+    name: string;
+    description: string;
+    category: string;
+    price: number;
+    readyTime: number;
+    image: [string];
+}
+
+//Restaurant Model
+export interface Restaurant {
+    _id: string;
+    name: string;
+    foodType: string;
+    address: string;
+    phone: string;
+    images: string;
+    foods: [FoodModel];
+}
