95f41000802d41019cddf7154b9f49e071f6b4aa
Mike Ramos <michaelangeloram8@gmail.com>
2023-02-19 12:47:14 -0600

    import FoodAvailability fr models

--- src/redux/actions/shoppingActions.ts
+++ src/redux/actions/shoppingActions.ts
@@ -2,4 +2,9 @@ import axios from 'axios';
 import { LocationGeocodedAddress } from 'expo-location';
 import { Dispatch } from 'react';
 import { BASE_URL } from '../../utils';
+import { FoodAvailability } from '../models';
 
+export interface AvailabilityAction {
+    readonly type: 'ON_AVAILABILITY',
+    payload: 
+}
