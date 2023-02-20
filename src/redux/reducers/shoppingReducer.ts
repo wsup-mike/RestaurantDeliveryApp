ca087efa7b96f354229fb94a371292af02d71260
Mike Ramos <michaelangeloram8@gmail.com>
2023-02-20 09:56:45 -0600

    shoppingReducer-complete ShoppingReducer

--- src/redux/reducers/shoppingReducer.ts
+++ src/redux/reducers/shoppingReducer.ts
@@ -1,12 +1,22 @@
+import { InitialState } from "@react-navigation/native";
 import { ShoppingAction } from "../actions";
-import { FoodAvailability } from "../models";
+import { FoodAvailability, ShoppingState } from "../models";
 
 const initialState = {
-    availability: {} as FoodAvailability;
+    availability: {} as FoodAvailability
 }
 
-const ShoppingReducer = () => {
-
+const ShoppingReducer = (state: ShoppingState = initialState, action: ShoppingAction) => {
+    switch(action.type) {
+        case 'ON_AVAILABILITY':
+            return {
+                ...state,
+                availability: action.payload
+            }
+        
+        default:
+            return state
+    }
 }
 
 export { ShoppingReducer }
