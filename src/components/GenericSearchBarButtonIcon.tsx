5a7f98a0ea97321bc3809984f0e2b6156899e5a7
Mike Ramos <michaelangeloram8@gmail.com>
2023-02-22 17:52:28 -0600

    Cr8 ButtonProps interface, deconstruct them, add w

--- src/components/GenericSearchBarButtonIcon.tsx
+++ src/components/GenericSearchBarButtonIcon.tsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, TextInput, Image } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, TextInput, Image, ImageSourcePropType } from 'react-native';
 
 interface ButtonProps { 
     onTap: Function;
     width: number;
-
+    height: number;
+    icon: ImageSourcePropType;
  }
 
-const GenericSearchBarButtonIcon: React.FC<ButtonProps> = ({  }) => {
+const GenericSearchBarButtonIcon: React.FC<ButtonProps> = ({  onTap, width, height, icon }) => {
+    
     return (
-        <TouchableOpacity style={[styles.btn, { }]}>
+        <TouchableOpacity style={[styles.btn, { width, height }]}>
         </TouchableOpacity>
     )
 };
